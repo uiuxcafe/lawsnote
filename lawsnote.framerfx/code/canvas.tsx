e6646ad874874f43dc8b3b1bfb6c03e4294fe456
Doris <daan9808114@gmail.com>
2019-10-05 10:52:55 +0800

    test

---
+++ lawsnote.framerfx/code/canvas.tsx
@@ -0,0 +1,73 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const $2_title = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_onJ3PTZOS", {}, 330,107);
+export const $2_title_select = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_gOvVdaG1M", {}, 330,107);
+export const BoxIcon = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_zm3r6z9UE", {}, 18,18);
+export const Card_filter = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_CHOBaoorO", {}, 260,239);
+export const Change_list = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Title?:string,$5?:string}>(canvas, "id_aZeX_U6lu", {Title:"string",$5:"string"}, 330,52);
+export const CheckIcon_filter = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_Ce905HlJn", {}, 18,18);
+export const Detail_00 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_IRHKhWwCb", {}, 668,704);
+export const Detail_01 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_lZ7n3On3k", {}, 668,704);
+export const Detail_02 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_DMp7bqBLi", {}, 668,704);
+export const Detail_03 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_K0mDzaaWx", {}, 668,704);
+export const Detail_04 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_MefAz3rZS", {}, 668,703);
+export const Dropdown = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_vhVa5lokO", {}, 190,33);
+export const Dropdown_list = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_ItkCeMxR4", {}, 190,156);
+export const Icon_todo_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_rEepGi_VJ", {}, 46,46);
+export const Icon_todo_2 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_OyfILxZ35", {}, 46,46);
+export const Law = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Title?:string,$5?:string}>(canvas, "id_TwebZAt6G", {Title:"string",$5:"string"}, 330,104);
+export const Law_list = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,$10?:string,Hello?:string}>(canvas, "id_TJVYtAok8", {$10:"string",Hello:"string"}, 330,40);
+export const Section_title_datatime = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_AthsEA_X7", {}, 330,26);
+export const Task_card_01 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_T_UeSsqYZ", {}, 296,164);
+export const Task_card_02 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_qEFkFmAvQ", {}, 296,164);
+export const Task_card_03 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_HFBlwdFQr", {}, 296,164);
+export const Task_card_03_1 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_VqOA3cnQb", {}, 296,164);
+export const Task_card_04 = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_Lab3c0z91", {}, 296,164);
+export const Task_card_hover = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_FelPom4G9", {}, 280,164);
+export const Task_title = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string,Title?:string,$5?:string}>(canvas, "id_H0Ww7Bjh6", {Title:"string",$5:"string"}, 200,30);
+
+export const colors = Object.freeze({
+    /** #151B25 */
+    "B100": "var(--token-706833f6-83c5-4844-9ab7-d06c60af088c, rgb(21, 27, 37))",
+    /** #20242C */
+    "B90": "var(--token-965d893a-9219-40c9-946a-21148f51b1b5, rgb(32, 36, 44))",
+    /** #242831 */
+    "B80": "var(--token-a1aca6ee-fabc-49f2-b540-1e70f1b7fc5f, rgb(36, 40, 49))",
+    /** #99A2A8 */
+    "B50": "var(--token-9fbb6e41-6055-4da6-bf34-83a9cb8d769b, rgb(153, 162, 168))",
+    /** #BFC4C7 */
+    "B30": "var(--token-0c966797-08f3-41e3-90ad-97683b36d281, rgb(191, 196, 199))",
+    /** #EBEDEF */
+    "B20": "var(--token-9fb86329-0d51-4bd7-88a5-6357d1530c0e, rgb(235, 237, 239))",
+    /** #FFFFFF */
+    "White": "var(--token-bf81bf4d-00e9-4570-bf60-cda5b1008b08, rgb(255, 255, 255))",
+    /** #F9FBFC */
+    "bg_Light": "var(--token-a0da50c1-eeb4-4d42-be6a-4e373e57104f, rgb(249, 251, 252))",
+    /** #F6F8F9 */
+    "bg_normal": "var(--token-ea942a84-8fc2-45d7-a27b-7db33f38affe, rgb(246, 248, 249))",
+    /** #E6E6E6 */
+    "Div": "var(--token-67587827-3539-4d4f-a849-af6356447e8c, rgb(230, 230, 230))",
+    /** #0F73F2 */
+    "P50": "var(--token-d36a9b1a-13c2-4d7e-bb8d-6825aae78069, rgb(15, 115, 242))",
+    /** #B9BBBE */
+    "text_gray": "var(--token-ba42fd30-d2ed-4386-910b-8fdf86bd6732, rgb(185, 187, 190))",
+    /** #FF5E87 */
+    "status_red": "var(--token-637aa239-750c-4ac9-a7ee-50e4c4d8245f, rgb(255, 94, 135))",
+    /** #37C5AB */
+    "status_green": "var(--token-8e0f8aee-0f17-4815-8d78-09feeb0abcde, rgb(55, 197, 171))",
+    /** #FFA265 */
+    "ststus_orange": "var(--token-7c728a2e-9dac-4922-8ddc-290ff96b041f, rgb(255, 162, 101))",
+    /** #6F79F0 */
+    "status_purple": "var(--token-4ff3a9d3-9bc3-4ce2-ab8f-7ae613593b9e, rgb(111, 121, 240))",
+    /** #4AC3FF */
+    "nav_blue": "var(--token-38becb18-2a88-42f4-9af4-16617601b3cb, rgb(74, 195, 255))",
+    /** #FF5E5E */
+    "Alert": "var(--token-ec389dd4-617a-4416-988a-48595918ea73, rgb(255, 94, 94))",
+    /** #00969E */
+    "green": "var(--token-16d3e164-486a-4c2e-80ac-e68143c59971, rgb(0, 150, 158))",
+    /** #00B8C2 */
+    "nav_blue_!": "var(--token-75b1c2bd-a061-4c7b-87db-8a93c8d54167, rgb(0, 184, 194))",
+    /** #0F9A49 */
+    "button_green": "var(--token-27937b97-4426-406e-93c0-5012367c15f7, rgb(15, 154, 73))",
+})
